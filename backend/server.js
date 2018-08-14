291cd53d8d909d27caa06da92ec2ddf5965e8d30
namdaoduy <lenkc98@gmail.com>
2018-08-14 11:02:46 +0700

    Client get real data

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import bodyParser from 'body-parser';
 import logger from 'morgan';
 import mongoose from 'mongoose';
 import { getSecret } from './secrets';
+import Comment from './models/comment';
 
 // and create our instances
 const app = express();
@@ -14,7 +15,7 @@ const router = express.Router();
 // set our port to either a predetermined port number if you have set it up, or 3001
 const API_PORT = process.env.API_PORT || 3001;
 
-mongoose.connect(getSecret('dbUri'));
+mongoose.connect(getSecret('dbUri'), { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
@@ -26,6 +27,32 @@ app.use(logger('dev'));
 // now we can set the route path & initialize the API
 router.get('/', (req, res) => {
   res.json({ message: 'Hello, World!' });
+});
+
+router.get('/comments', (req, res) => {
+  Comment.find((err, comments) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: comments });
+  });
+});
+
+router.post('/comments', (req, res) => {
+  const comment = new Comment();
+  // body parser lets us use the req.body
+  const { author, text } = req.body;
+  if (!author || !text) {
+    // we should throw an error. we can do this check on the front end
+    return res.json({
+      success: false,
+      error: 'You must provide an author and comment'
+    });
+  }
+  comment.author = author;
+  comment.text = text;
+  comment.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
 });
 
 // Use our router configuration when we call /api
