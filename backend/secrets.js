72bdd6a5866f14e21988d7a7aa94bc789fa4360a
Đào Duy Nam <lenkc98@gmail.com>
2018-08-14 09:12:48 +0700

    Delete secrets.js

--- backend/secrets.js
+++
@@ -1,7 +0,0 @@
-// secrets.js
-const secrets = {
-  // dbUri: process.env.DB_URI
-  dbUri: "mongodb://nam21031998:nam21031998@ds113692.mlab.com:13692/mern-comment-box"
-};
-
-export const getSecret = key => secrets[key];
